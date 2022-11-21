564dd240dd881fc0cc83a789c812dd0abbde9645
Jonathan Hellqvist <newfrontab@gmail.com>
2022-11-21 13:59:52 +0100

    Sunny days test for retrieving blogpost

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const express = require("express");
 const app = express();
 
+const blogPosts = [{id: 1, title: "First blogpost hello"}];
+
 app.get("/", (req, res) => {
   res.status(200).json({
     ok: true
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const blogPostIdParam = req.params.id;
+  const blogPost = blogPosts.find((b) => b.id.toString() === blogPostIdParam);
+  res.status(200).json({
+    blogPost: !!blogPost
   });
 });
 
--- tests/basic-test.js
+++ tests/basic-test.js
@@ -17,3 +17,18 @@ describe("GET /", () => {
       });
   });
 });
+
+
+describe("GET /:id", () => {
+  it("ID GET request should return blogpost", () => {
+    return request(app)
+      .get("/1")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((response) => {
+        response.body.should.eql({
+          blogPost: true
+        });
+      });
+  });
+});
